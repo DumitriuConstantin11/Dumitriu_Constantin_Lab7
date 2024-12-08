c820d8cc1e724cbab564c345f3d8633851eb6138
DumitriuConstantin11 <titirex22@gmail.com>
2024-12-08 22:10:03 +0200

    laborator9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -13,7 +13,32 @@ namespace DumitriuConstantinLab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product) 
+        { 
+            return _database.DeleteAsync(product);        
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
@@ -38,6 +63,38 @@ namespace DumitriuConstantinLab7.Data
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
             return _database.DeleteAsync(slist);
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
+        public Task<ListProduct> GetListProductAsync(int shopListID, int productID)
+        {
+            return _database.Table<ListProduct>()
+                .Where(lp => lp.ShopListID == shopListID && lp.ProductID == productID)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<int> DeleteListProductAsync(ListProduct listProduct)
+        {
+            return _database.DeleteAsync(listProduct);
         }
     }
 }
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -20,5 +20,44 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList) this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    async void OnChooseDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (listView.SelectedItem != null)
+        {
+            var selectedProduct = listView.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+                var currentShopList = (ShopList)BindingContext;
+
+                var listProduct = await App.Database.GetListProductAsync(currentShopList.ID, selectedProduct.ID);
+                if (listProduct != null)
+                {
+                    await App.Database.DeleteListProductAsync(listProduct);
+
+                    listView.ItemsSource = await App.Database.GetListProductsAsync(currentShopList.ID);
+                }
+            }
+            await Navigation.PopAsync();
+        }
+    }
+
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 
 }
